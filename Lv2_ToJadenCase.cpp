c505fdae4ddde6854dc74a2da06eca2d13985fa7
202331044 <131847052+202331044@users.noreply.github.com>
2025-09-10 10:50:52 +0900

    feat: solve #Lv2 - 4. JadenCase 문자열 만들기

---
+++ Lv2_ToJadenCase.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+string solution(string s) {
+    
+    for(int i = 0; i < s.size(); ++i)
+    {
+        if(i == 0 || s[i - 1] == ' ')
+            s[i] = toupper(s[i]);
+        else
+            s[i] = tolower(s[i]);
+    }
+    string answer = s;
+    return answer;
+}
