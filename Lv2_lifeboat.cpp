2049427163fade84bcae2115335ca905edb0f934
202331044 <131847052+202331044@users.noreply.github.com>
2025-09-18 12:41:08 +0900

    feat: solve Lv2 - 12. 구명보트

---
+++ Lv2_lifeboat.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    
+    sort(people.begin(), people.end());
+    int answer = 0;
+    int left = 0, right = people.size()-1;
+  
+    while(left <= right)
+    {
+        if(people[left] + people[right] <= limit)
+        {
+            answer++;
+            left++;
+            right--;
+        }
+        else
+        {
+            answer++;
+            right--;
+        }
+    }
+  
+    return answer;
+}
