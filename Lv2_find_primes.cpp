484c4b0e1ebd84d62ebac7422efb8af53fe2c788
202331044 <131847052+202331044@users.noreply.github.com>
2025-11-06 11:01:02 +0900

    feat: solve Lv2 - 56. 소수 찾기

---
+++ Lv2_find_primes.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+unordered_set<int> result;
+
+bool prime_number(int num)
+{
+    if(num <= 1)
+        return false;
+    
+    for(int i = 2; i <= num/2; ++i)
+    {
+        if(num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void bf(string str, string& numbers, vector<bool>& vec)
+{
+    if(str.size() > 0)
+    {
+        int num = stoi(str);
+        if(prime_number(num))
+            result.insert(num);
+    }
+    
+    if(str.size() >= numbers.size())
+        return;
+    
+    for(int i = 0; i < numbers.size(); ++i)
+    {
+        if(vec[i])
+            continue;
+        
+        vec[i] = true;
+        bf(str + numbers[i], numbers, vec);
+        vec[i] = false;
+    }
+}
+
+int solution(string numbers) {
+    string str;
+    vector<bool> vec(numbers.size(), false);
+    
+    bf(str, numbers, vec);
+    int answer = result.size();
+    
+    return answer;
+}
